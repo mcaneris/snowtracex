764d6d94e53a23a1ecb074552f64410dbeb951af
Mete Can Eris <can.eris@feyn.dev>
2022-01-23 15:12:31 +0300

    Init project

---
+++ lib/snowtracex.ex
@@ -0,0 +1,18 @@
+defmodule Snowtracex do
+  @moduledoc """
+  Documentation for `Snowtracex`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Snowtracex.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/snowtracex_test.exs
@@ -0,0 +1,8 @@
+defmodule SnowtracexTest do
+  use ExUnit.Case
+  doctest Snowtracex
+
+  test "greets the world" do
+    assert Snowtracex.hello() == :world
+  end
+end
