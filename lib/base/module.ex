50ca2ca8e20587721e3b4f813b05f8b3f6979f1b
Mete Can Eris <can.eris@feyn.dev>
2022-01-25 12:39:16 +0300

    Implement base module

--- lib/base/module.ex
+++ lib/base/module.ex
@@ -1,9 +1,10 @@
-defmodule Snowtracex.Module do
+defmodule Snowtracex.APIModule do
   @moduledoc false
 
   defmacro __using__(_opts) do
     quote do
       alias Snowtracex.Client
+      require Logger
     end
   end
 end
